b76534c8ccfa9793e9fe9344c4986ce069ba9cab
UnLuckyLust <104845736+UnLuckyLust@users.noreply.github.com>
2022-11-04 17:27:57 +0200

    Create fxmanifest.lua

---
+++ ull-watermark/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+game 'gta5'
+lua54 'yes'
+
+author 'UnLuckyLust#9534'
+description 'WaterMark for FiveM servers'
+version '1.0.0'
+
+ui_page '/nui.html'
+
+files {
+    '/nui.html',
+    '/style.css',
+    '/logo.png',
+}
